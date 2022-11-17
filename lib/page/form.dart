07cc584c84d2da40d7f90df403cce33e3c56c092
David Christianto <christianto09@Alberts-MacBook-Pro.local>
2022-11-17 10:12:35 +0700

    Lab08

--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,4 +1,5 @@
 import 'package:pbp_lab06/main.dart';
+import 'package:pbp_lab06/page/to_do_page.dart';
 import 'package:flutter/material.dart';
 
 class MyFormPage extends StatefulWidget {
@@ -65,6 +66,16 @@ class _MyFormPageState extends State<MyFormPage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route the menu to the to do page
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
